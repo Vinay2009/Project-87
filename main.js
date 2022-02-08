55812843aa488ac4b80ddb57d2c3ab35d10a5a63
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-02-08 21:58:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,31 +30,31 @@ function my_keydown(e)
 keyPressed = e.keyCode;
 console.log(keyPressed);
 
-	if(keyPressed == '?') // add appropriate keycode
+	if(keyPressed == '82')
 	{
-		// upload red ranger
+		new_image('rr.jpg');
+		console.log("r");
 	}
 	if(keyPressed == '71')
 	{
-		block_x = 200;
-		// upload green ranger
-	}
-	
+	  new_image('gr.png');
+            console.log("g");
+    }
 	if(keyPressed == '89')
 	{
-		block_x =350;
-		// upload yellow ranger
-	}
+            new_image('yr.png');
+            console.log("y");
+    }
 	if(keyPressed == '80')
 	{
-		block_x = 600;
-		// upload pink ranger
-	}
+            new_image('pr.png');
+            console.log("p");
+    }
 	if(keyPressed == '66')
 	{
-		block_x = 700;
-	// upload blue ranger
-	}
+            new_image('br.png');
+            console.log("b");
+    }
 	
 }
 
